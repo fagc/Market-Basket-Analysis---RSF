cb29eb0fdaafc597d7e96900aed905ccae111438
snigdha upadhyay <snigdhaupadhyay6@gmail.com>
2024-10-30 22:46:39 -0500

    addted code to download dataset, ds cleanup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-#Just the folder structure to get things started
+import kagglehub
+import pandas as pd
+#Uncomment to download the dataset
+# Download latest version
+# path = kagglehub.dataset_download("mkechinov/ecommerce-purchase-history-from-electronics-store")
+#path = kagglehub.dataset_download("datafiniti/electronic-products-prices")
+# print("Path to dataset files:", path)
+
+ds1 = "./dataset/DataSet1.csv"  
+ds2 = "./dataset/DataSet2.csv"  
+# Read the CSV file
+dataset1 = pd.read_csv(ds1)
+dataset2 = pd.read_csv(ds2)
+
+#data cleanup
+
+# Remove duplicate rows
+data1_cleaned = dataset1.drop_duplicates()
+data2_cleaned = dataset2.drop_duplicates()
+
